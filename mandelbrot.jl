195a381825cf03ce909fb1c64383aa38ed1206dd
amandamac0912 <48692678+amandamac0912@users.noreply.github.com>
2019-03-25 15:49:22 -0300

    Update mandelbrot.jl

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -6,8 +6,17 @@ using Images
 # z^2 = x^2 + 2xyi - y^2 = (x^2 - y^2) + (2xy) i
 function iteracao_mandelbrot(cx, cy; max_iter = 40)
   iter = 0
-
-  # AQUI
+  x = 0
+  y = 0
+  
+  while iter < 40
+    re = x^2-y^2+cx
+    imag =2*x*y+cy
+        iter = iter +1
+        x = re
+        y = imag
+end
+ 
 
   if x^2 + y^2 < 4
     return false, 0
